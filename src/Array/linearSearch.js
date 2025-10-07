23a25a32eeb06a9b57113b2a8322e22163d452fb
Vishnu <vishnu.cp.13@gmail.com>
2025-10-07 14:04:20 +0530

    Added Linear search

---
+++ src/Array/linearSearch.js
@@ -0,0 +1,16 @@
+let arr = [10,20,32,1,6];
+let target = 1;
+
+function linear(arr,target){
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==target){
+
+        return i
+    }
+    
+}
+return -1
+
+}
+console.log(linear(arr,target));
