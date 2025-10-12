62ca4e0214a12b53156bd784019524041658d32c
Vishnu <vishnu.cp.13@gmail.com>
2025-10-13 00:03:16 +0530

    Added movezero

---
+++ src/Array/movezero.js
@@ -0,0 +1,24 @@
+let arr=[0, 1, 0, 3, 12]
+
+
+function movezero(arr){
+    let j = 0;
+for (let i = 0; i < arr.length; i++) {
+  if(arr[i]!=0){
+    arr[j]=arr[i];
+    j++
+}
+
+
+}
+  
+while(j<arr.length){
+    arr[j]=0;
+    j++
+}
+
+return arr
+
+}
+
+console.log(movezero(arr));
