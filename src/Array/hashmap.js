0ce05fe9265c7220a81bb0483975b00193f999bf
Vishnu <vishnu.cp.13@gmail.com>
2025-10-14 23:11:15 +0530

    learnd hashmap

--- src/Array/hashmap.js
+++ src/Array/hashmap.js
@@ -1,4 +1,21 @@
-let map = new Map ()
-map.set("hello", 45)
 
-console.log(map.get("hello"));
+let nums = [2, 7, 11, 15];
+let target = 9;
+
+function hashm(nums,target){
+   let map = new Map();
+
+   for(let i=0;i<nums.length;i++){
+    let balance = target-nums[i];
+
+    if(map.has(balance)){
+        return [map.get(balance),i]
+    }
+     map.set(nums[i],i)
+   }
+   
+
+}
+
+
+console.log(hashm(nums,target));
