1428c186928b55369618a16df219503934f37c39
Vishnu <vishnu.cp.13@gmail.com>
2025-10-17 23:43:43 +0530

    learned binary search

--- src/Array/binarySearch.js
+++ src/Array/binarySearch.js
@@ -1,28 +1,22 @@
-let arr =[1,10,23,45,67,89,100];
-let target = 45;
+let arr =[1,2,3,4,5,10,20,30,40,50];
+let target=20;
 
 function binarySearch(arr,target){
+  let low=0;
+  let high=arr.length-1;
 
-
-    let low= 0;
-    let high = arr.length-1;
-    while(low<=high){
-        let mid = Math.floor((low+high)/2);
-
-        if(target == arr[mid]){
-            return mid
-        }else if (target>mid){
-            low = mid+1
-        }else{
-            high=mid-1
-        }
+  while(low<=high){
+    let mid=Math.floor((low+high)/2);
+    
+    if(target==arr[mid]){
+      return mid
+    }else if(target<=arr[mid]){
+      high=mid-1
+    }else{
+      low=mid+1
     }
-    return -1
+  }
+  return -1
 }
 
 console.log(binarySearch(arr,target));
-
-// Big O(logn)
-
-
-
---
+++ src/Array/first&LastOccurence.js
@@ -0,0 +1,62 @@
+let arr=[1,2,3,4,5,5,5,5];
+let target = 5;
+
+
+
+function occurence(arr,target){
+
+    function first(arr,target){
+let low=0;
+let high=arr.length-1;
+let first=-1
+
+while(low<=high){
+    let mid=Math.floor((low+high)/2);
+
+    if(arr[mid]==target){
+        first=mid;
+        high=mid-1
+    }
+
+    else if(target<=arr[mid]){
+        high=mid-1
+    }else{
+        low=mid+1
+    }
+}
+         return first
+    }
+
+function last(arr,target){
+
+        let low=0;
+let high=arr.length-1;
+let last=-1
+
+while(low<=high){
+    let mid=Math.floor((low+high)/2);
+
+    if(arr[mid]==target){
+        last=mid;
+        low=mid+1
+    }
+
+    else if(target<=arr[mid]){
+        high=mid-1
+    }else{
+        low=mid+1
+    }
+}
+         return last
+
+
+    }
+
+    return [first(arr,target),last(arr,target)]
+
+}
+
+
+
+
+console.log(occurence(arr,target));
